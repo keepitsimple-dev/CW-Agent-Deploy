087d8e530591a42762e4166054ed70991c3ff4f2
lroberts-kits <71227961+lroberts-kits@users.noreply.github.com>
2020-11-19 09:27:14 -0600

    Update Agent_Install.bat

--- Agent_Install.bat
+++ Agent_Install.bat
@@ -4,11 +4,11 @@ If EXIST c:\windows\ltsvc\ltsvc.exe GOTO EXIT
 GOTO INSTALL
 
 :INSTALL
-mkdir C:\AGENTSHARE
-cd C:\AGENTSHARE && curl -LJO https://github.com/keepitsimple-dev/CW-Agent-Deploy/raw/main/Agent_Install.exe
-call C:\AGENTSHARE\Agent_Install.exe /s
-del /F /Q C:\AGENTSHARE
-rmdir /Q /S C:\AGENTSHARE
+mkdir C:\KITS_AGENT
+cd C:\KITS_AGENT && curl -LJO https://github.com/keepitsimple-dev/CW-Agent-Deploy/raw/main/Agent_Install.exe
+call C:\KITS_AGENT\Agent_Install.exe /s
+del /F /Q C:\KITS_AGENT
+rmdir /Q /S C:\KITS_AGENT
 
 GOTO EXIT
 
